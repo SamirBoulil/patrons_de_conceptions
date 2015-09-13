bc74dedac974efd10c5ca8f17a227e8b98ac0d33
Samir Boulil <samir.boulil@lengow.com>
2015-09-13 15:48:21 +0200

    Decorator: Adds definition

--- decorator/main.py
+++ decorator/main.py
@@ -4,6 +4,13 @@ from boisson import CafeNoir
 from condiment_decorateur import Chantilly
 
 
+"""
+Decorator pattern: The decorator pattern attaches additionnal responsibilities
+to an object dynamically. Decorators provide flexible alternative to subclassing
+for extending functionnality.
+"""
+
+
 def main():
     """ Implémentation du patron de conception décorateur
     Des cafés et des condiments.

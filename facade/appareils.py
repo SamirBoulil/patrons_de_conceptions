7b17fa2c4a2208113f8abcb973afb0418fb9cced
Samir Boulil <samir.boulil@lengow.com>
2015-09-17 16:40:44 +0200

    Facade: first commit.
    - Add devices class
    - Add main

---
+++ facade/appareils.py
@@ -0,0 +1,37 @@
+# coding: utf-8
+
+
+""" Un ensemble d'appareils avec lesquels nous pouvons intéragir.
+"""
+
+
+class Amplifier:
+    pass
+
+
+class Tuner:
+    pass
+
+
+class CDPlayer:
+    pass
+
+
+class DVDPlayer:
+    pass
+
+
+class Screen:
+    pass
+
+
+class Projector:
+    pass
+
+
+class TheaterLights:
+    pass
+
+
+class PopcornMachine:
+    pass
---
+++ facade/main.py
@@ -0,0 +1,23 @@
+# coding: utf-8
+
+
+""" Gestion d'un home cinéma à l'aide du pattern facade.
+
+Notre sous-système correspond à tous les éléments nous permettant d'intéragir
+avec un device, amplifieur, TV, sono, magnetoscope, lecteur DVD.
+
+Le pattern facade nous permet d'unifié l'utilisation de tous ces appareils au
+sein d'une seule interface qui simplifie l'utilisation et la gestion.
+
+Facade design pattern: provides a unified interface to a set of interfaces in a
+subsystem. Facade defines a higher level of interface that makes the subsystem
+easier to use.
+"""
+
+
+def main():
+    """
+    """
+
+if __name__ == "__main__":
+    main()
